c309fe684194dd42377c9add767472c65fbbb0ed
a5810130 <a5810130@gmail.com>
2017-04-14 20:18:56 +0700

    created model

--- buswait/models.py
+++ buswait/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Route(models.Model):
+    bus_number = models.CharField(max_length=10)
+    
+class BusStop(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    position = models.IntegerField('positon')
+    bus_terminus = models.BooleanField()
+    
+class PassedTime(models.Model):
+    busStop = models.ForeignKey(BusStop, on_delete=models.CASCADE)
+    time = models.DateTimeField('passed_time')
+    
+    def bus_number(self):
+        return self.busStop.route.bus_number
