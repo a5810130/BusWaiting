eab5267b211edf06b0243de7b1c50b687c0630c7
a5810130 <a5810130@gmail.com>
2017-04-19 01:50:48 +0700

    add "import reverse"

--- buswait/views.py
+++ buswait/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.utils import timezone 
+from django.urls import reverse
 
 from buswait.models import *
 
